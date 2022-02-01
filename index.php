6abf1cf0e53efe887802fd34176c0bd0bd4cf4a8
josh-delaCruz <jommel.delacruzb@outlook.it>
2022-02-01 15:58:57 +0100

    catalogoV2

--- index.php
+++ index.php
@@ -3,31 +3,45 @@
     <title>Prototipo</title>   
     
     <!-- componenti -->
-    <link rel="stylesheet" href="style.css">
-    
+    <link rel="stylesheet" href="./css/styles.css">
+
+    <!-- font-family: ubuntu bold-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+
 </head>   
 <body>
 
 
-    <h1>Test</h1>
-    <p>*Nome prodotto</p>
+    <h1 class="text">Test</h1>
     
     <table>
         
-    
     <tr>
-    <td>
+        <div>
+            <td colspan="2"><p class="text">*Nome prodotto</p></td>
+        </div>
+    </tr>
+    <tr>
+    <td colspan="2">
         <div>    
-            <img src="images/holeder.PNG" >
+            <img class="image" src="images/holeder.PNG" >
         </div>
     </td>
     </tr>  
-        <td>Descrizione</td>
-        <td>*Inderire descrizione prodotto</td>
+        <td><p class="text">Descrizione:</p></td>
+        <td><p class="text">*Inserire descrizione prodotto</p></td>
     <tr>
-        <td><a href="pages/viewer.html">360° view</a></td>
+    <tr>
+        <td><p>Prezzo: </p></td>
+        <td><p>*numero</p></td>
+    </tr>
+        <!--<td colspan="2"><a href="pages/viewer.html" class="text">360° view</a></td>-->
+        <td colspan="2"><button type="button" onclick="location.href='./pages/viewer.html'">360° view</button><td>
     </tr>
    
 </table>
+
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-
-
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
-
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
