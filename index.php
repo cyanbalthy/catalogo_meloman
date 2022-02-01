f4b5f0ddfb873f6d825af0fc8b7f50c846020fcf
josh-delaCruz <jommel.delacruzb@outlook.it>
2022-02-01 12:33:07 +0100

    catalogoV1

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <title>Prototipo</title>   
+    
+    <!-- componenti -->
+    <link rel="stylesheet" href="style.css">
+    
+</head>   
+<body>
+
+
+    <h1>Test</h1>
+    <p>*Nome prodotto</p>
+    
+    <table>
+        
+    
+    <tr>
+    <td>
+        <div>    
+            <img src="images/holeder.PNG" >
+        </div>
+    </td>
+    </tr>  
+        <td>Descrizione</td>
+        <td>*Inderire descrizione prodotto</td>
+    <tr>
+        <td><a href="pages/viewer.html">360° view</a></td>
+    </tr>
+   
+</table>
+</body>
+</html>
